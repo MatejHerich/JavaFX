779b83fbdb8286ba6f80b8f7ca7f23f2708f1052
Matej Herich <herich.matej2005@gmail.com>
2025-10-03 14:46:55 +0200

    Update of Color Mixer

--- mix_farieb/src/main/java/com/example/mix_farieb/HelloController.java
+++ mix_farieb/src/main/java/com/example/mix_farieb/HelloController.java
@@ -10,7 +10,9 @@ public class HelloController {
     @FXML
     private Rectangle colorBox;
     private int red = 0,green = 0,blue = 0;
-    Color color;
+    private Color color;
+    @FXML
+    private Label redlabel, greenlabel, bluelabel;
 
     private void updateColor() {
         colorBox.setFill(Color.rgb(red, green, blue));
@@ -27,36 +29,42 @@ public class HelloController {
         red += 5;
         red = Math.min(255, Math.max(0, red));
         updateColor();
+        redlabel.setText("RED: " + red);
     }
     @FXML
     public void redminus() {
         red -= 5;
         red = Math.min(255, Math.max(0, red));
         updateColor();
+        redlabel.setText("RED: " + red);
     }
     @FXML
     public void greenplus() {
         green += 5;
         green = Math.min(255, Math.max(0, green));
         updateColor();
+        greenlabel.setText("GREEN: " + green);
     }
     @FXML
     public void greenminus() {
         green -= 5;
         green = Math.min(255, Math.max(0, green));
         updateColor();
+        greenlabel.setText("GREEN: " + green);
     }
     @FXML
     public void blueplus() {
         blue += 5;
         blue = Math.min(255, Math.max(0, blue));
         updateColor();
+        bluelabel.setText("BLUE: " + blue);
     }
     @FXML
     public void blueminus() {
         blue -= 5;
         blue = Math.min(255, Math.max(0, blue));
         updateColor();
+        bluelabel.setText("BLUE: " + blue);
     }
 
 }
